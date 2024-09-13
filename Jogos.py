86be5b5a7e5879046bb186e8a6538f009e42bea9
pescocofino <134400750+pescocofino@users.noreply.github.com>
2024-09-13 10:41:03 -0300

    Add files via upload

--- Jogos.py
+++ Jogos.py
@@ -1,25 +1,30 @@
 class Jogos:
     jogos = []  
 
-    def __init__(self, nome, desenvolvedor, genero, ano_lancamento):
+    def __init__(self, nome, desenvolvedor, genero, ano_lancamento, estoque):
         self.nome = nome
         self.desenvolvedor = desenvolvedor
         self.genero = genero
         self.ano_lancamento = ano_lancamento
+        self._estoque = estoque
+
         Jogos.jogos.append(self)
 
     def __str__(self):
-        return (f'{self.nome} | {self.desenvolvedor} | {self.genero} | {self.ano_lancamento}')
+        return (f'{self.nome} | {self.desenvolvedor} | {self.genero} | {self.ano_lancamento} | {self.estoque}')
 
     @staticmethod
     def listar_jogos():
+        
         for jogo in Jogos.jogos:
             print(jogo)
 
+    @property
+    def estoque(self):
+        return 'Sem estoque' if not self._estoque else 'Em estoque'
 
-jogo_The_Witcher = Jogos("The Witcher 3: Wild Hunt", "CD Projekt Red", "RPG", 2015)
-jogo_Cyberpunk = Jogos("Cyberpunk 2077", "CD Projekt Red", "RPG", 2020)
-jogo_Hades = Jogos("Hades", "Supergiant Games", "Roguelike", 2020)
-
+jogo_The_Witcher = Jogos("The Witcher 3: Wild Hunt", "CD Projekt Red", "RPG", 2015, True)
+jogo_Cyberpunk = Jogos("Cyberpunk 2077", "CD Projekt Red", "RPG", 2020, False)
+jogo_Hades = Jogos("Hades", "Supergiant Games", "Roguelike", 2020, True)
 
 Jogos.listar_jogos()
